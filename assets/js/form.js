501f67601c453d6aab41ff9503fb26a95e5854c3
Kevin <kevhoang97@gmail.com>
2024-06-17 22:39:05 -0400

    added light mode and dark mode

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,8 +2,12 @@ const usernameInput = document.querySelector('#username-input');
 const titleInput = document.querySelector('#title-input');
 const contentInput = document.querySelector('#content-input');
 const submitBtn = document.querySelector('#submit');
+const pageMode = document.querySelector('.dark');
+const changeMode = document.querySelector('#changeMode')
 
-let tempBlogObject = []
+let tempBlogObject = [];
+
+let mode = 'dark';
 
 function updateLocalStorage() {
     localStorage.setItem('storageInfo', JSON.stringify(tempBlogObject));
@@ -31,3 +35,29 @@ submitBtn.addEventListener('click', function (event) {
 
     location.href = "blog.html"
 });
+
+changeMode.addEventListener('click', function (){
+    if (mode === 'dark') {
+        mode = 'light';
+        pageMode.setAttribute('class','light')
+
+        if(changeMode.innerText == "Light") {
+            changeMode.innerText = "Dark"
+        }
+        else {changeMode.innerText = "Light"}
+    }
+    
+    else {
+        mode = 'dark'
+        pageMode.setAttribute('class', 'dark')
+        
+        if(changeMode.innerText == "Dark") {
+            changeMode.innerText = "Light"
+        }
+        else {changeMode.innerText = "Dark"}
+    }
+})
+
+
+
+
